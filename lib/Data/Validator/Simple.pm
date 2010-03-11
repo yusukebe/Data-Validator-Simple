5f5e3f77adbcda3cd1ea840bd15ca304a53034c9
Yusuke Wada <yusukebe@cpan.org>
2010-03-11 11:14:23 +0900

    initial commit

---
+++ lib/Data/Validator/Simple.pm
@@ -0,0 +1,32 @@
+package Data::Validator::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Data::Validator::Simple -
+
+=head1 SYNOPSIS
+
+  use Data::Validator::Simple;
+
+=head1 DESCRIPTION
+
+Data::Validator::Simple is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
